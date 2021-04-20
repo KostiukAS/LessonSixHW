c3da7bc1cf4d0b1e9d08386e6f5f3548fd127877
KostiukAS <kostiukandrey2005@gmail.com>
2021-04-20 08:28:07 +0300

    Person struct

---
+++ LessonSixHW/PersonData.swift
@@ -0,0 +1,30 @@
+//
+//  PersonData.swift
+//  LessonSixHW
+//
+//  Created by admin on 20.04.2021.
+//
+
+import UIKit
+
+struct Person {
+    
+    // MARK: - Properties
+    
+    let collectionOfNames = ["Nick", "John", "Ron", "Aaron", "Martin"]
+    
+    let collectionOfSurnames = ["Smith", "Bond", "Livingstone", "Akerman", "Hudson"]
+    
+    let collectionOfEmails = ["qwerty@gmail.com",
+                              "super_programer45@gmail.com",
+                              "pomidorchik228@gmail.com",
+                              "photoshop_master97@gmail.com",
+                              "real_hitman200@gmail.com"]
+    
+    let collectionOfTelephones = ["0568790348",
+                                  "5867093458",
+                                  "7798456238",
+                                  "4067588253",
+                                  "0576877325"]
+    
+}
